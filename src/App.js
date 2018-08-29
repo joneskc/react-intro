4ffd616dbb21309da20fa1f7c60a3da9256e4ccb
Keith Jones <keithjones@Keiths-MacBook-Pro.local>
2018-08-29 12:01:26 -0600

    broke header into own component: Header.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+// CSS FOR ENTIRE APPLICATION
 import './App.css';
 
+// IMPORT COMPONENTS
+import Header from './components/Header'
+
+// APP.JS IS ALWAYS A CLASS COMPONENT, EXTENDING THE BASE REACT.COMPONENT
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
